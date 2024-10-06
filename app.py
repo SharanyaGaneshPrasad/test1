ac62ef297b6430d6c3c203f6b014fb8aaf3ae160
SharanyaGaneshPrasad <sharanya.prasad@digitalcareerinstitute.org>
2024-10-06 22:08:46 +0200

    Initial commit with README app.py

---
+++ app.py
@@ -0,0 +1,6 @@
+# app.py
+def main():
+    print("Welcome to the Python Multibranch Project")
+
+if __name__ == "__main__":
+    main()
---
+++ test_app.py
@@ -0,0 +1,10 @@
+# test_app.py
+import unittest
+from app import main
+
+class TestApp(unittest.TestCase):
+    def test_main(self):
+        self.assertEqual(main(), None)
+
+if __name__ == "__main__":
+    unittest.main()
